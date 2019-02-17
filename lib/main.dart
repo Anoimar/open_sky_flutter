0ae5426c5e89bb782b3bb3fdf1a4678f35f68a70
Anoimar <vladyslaw@yahoo.com>
2019-02-17 14:42:08 +0100

    added filter by country

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,30 @@ class _HomePageState extends State<HomePage> {
   Map data;
   List flightData;
 
+  List filteredData;
+
+  var _searchView = new TextEditingController();
+
+  bool _firstSearch = true;
+
+  String _querry = "";
+
+
+  _HomePageState(){
+    _searchView.addListener((){
+      if (_searchView.text.isEmpty){
+        setState(() {
+          _firstSearch = false;
+          _querry = "";
+        });
+      } else {
+        setState(() {
+          _firstSearch = false;
+          _querry = _searchView.text.toLowerCase();
+        });
+      }
+    });
+  }
 
   Future getData() async {
     http.Response response = await http.get("https://opensky-network.org/api/states/all");
@@ -42,6 +66,7 @@ class _HomePageState extends State<HomePage> {
     data = json.decode(response.body);
     setState(() {
       flightData = data["states"];
+      filteredData = flightData;
     });
   }
 
@@ -58,33 +83,71 @@ class _HomePageState extends State<HomePage> {
           title: Text("Open SKY"),
           backgroundColor: Colors.green,
         ),
-        body: ListView.builder(
-            itemCount: flightData == null ? 0 : flightData.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(0,0,8.0,0.0),
-                            child: Image(
-                              height: 48.0,
-                              width: 56.0,
-                              image: NetworkImage(
-                                  createCountryFlagUrl(flightData[index][2])),
-                            ),
-                          ),
-                          textField(flightData[index][2]),
-                          textField(flightData[index][1]),
-                          textField(flightData[index][0])
-                        ]
-                    ),
-                  )
-              );
-            })
+        body: new Container(
+          margin: EdgeInsets.only(left: 10.0,top: 10.0,right: 10.0  ),
+          child: new Column(
+            children: <Widget>[
+              _createSearchView(),
+              _firstSearch ? _createListView(): _performSearch()
+            ],
+          ),
+        )
     );
   }
+
+
+  
+  Widget _createListView(){
+    return Flexible(child:ListView.builder(
+        itemCount: filteredData == null ? 0 : filteredData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0,0,8.0,0.0),
+                        child: Image(
+                          height: 48.0,
+                          width: 56.0,
+                          image: NetworkImage(
+                              createCountryFlagUrl(filteredData[index][2])),
+                        ),
+                      ),
+                      textField(filteredData[index][2]),
+                      textField(filteredData[index][1]),
+                      textField(filteredData[index][0])
+                    ]
+                ),
+              )
+          );
+        })
+    );
+}
+
+  Widget _performSearch(){
+    print("Filtering");
+    List toFilter = new List.from(flightData);
+    toFilter.removeWhere((item) => !item[2].toString().toLowerCase().contains(_querry));
+    filteredData = toFilter;
+    return _createListView();
+  }
+
+  Widget _createSearchView() {
+    return new Container(
+      decoration: BoxDecoration(border: Border.all(width: 1.0)),
+      child: new TextField(
+        controller: _searchView,
+        decoration: InputDecoration(
+          hintText: "Write country name",
+          hintStyle: new TextStyle(color: Colors.grey)
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
 
   Widget textField(flightDataText){
     return Expanded(
@@ -102,6 +165,8 @@ class _HomePageState extends State<HomePage> {
     return "http://www.sciencekids.co.nz/images/pictures/flags680/${flagCountryName.replaceAll(" ", "_")}.jpg";
   }
 
+
 }
+
 
 
